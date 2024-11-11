f7c7eadc2faec64c8de6ec2f1311e94dbb0941bf
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-11-11 21:48:06 +0530

    day-6

---
+++ javascript/day-6/index.js
@@ -0,0 +1,42 @@
+// Javascript loops
+
+// *#### 1. **For Loop**
+// The `for` loop is used to repeat a block of code a known number of times.
+
+// 1 to 100 numbers print karwde
+
+// for (let i = 1; 6<=5; 5++ ){
+//     console.log(i);
+//     // 1
+//     // 2
+//     // 3
+//     // 4
+//     // 5
+// }
+
+
+let array = ["Hello" , "World" , "this is suraj"];
+
+for(let i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+
+
+ 
+// *2. while ( assignent) (post condition runner)
+
+let value = 1;
+
+ while(value === 0){
+  console.log(value);  
+ }
+
+
+// 3. do while (pre-condtion runner)
+
+
+let i = 10;
+do {
+console.log(i)
+}
+while( i === 0)
